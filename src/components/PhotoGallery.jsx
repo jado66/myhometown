6cd443137f260fd93dfa1221c4f577407a05facc
jado66 <77120464+jado66@users.noreply.github.com>
2024-05-16 17:11:31 -0600

    Added a lightbox

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Box from '@mui/material/Box';
@@ -6,10 +6,14 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Button from '@mui/material/Button';
 import UploadImage from '@/components/util/UploadImage';
-import { Image } from '@mui/icons-material';
+import { Image as ImageIcon } from '@mui/icons-material';
+import { Dialog } from '@mui/material';
+
 
 const PhotoGallery = ({isEdit, photos, changePhoto}) => {
+
   const theme = useTheme();
+  const [selectedImage, setSelectedImage] = useState();
 
   const isMd = useMediaQuery(theme.breakpoints.up('md'), {
     defaultMatches: true,
@@ -18,6 +22,16 @@ const PhotoGallery = ({isEdit, photos, changePhoto}) => {
   if (!photos) {
     return null;
   }
+
+
+  const openImageDialog = image => () => {
+    setSelectedImage(image);
+  };
+
+  const closeImageDialog = () => {
+    setSelectedImage(null);
+  };
+
 
   const handlePhotoChange = (url, key) => {
     changePhoto(url, key);
@@ -42,6 +56,8 @@ const PhotoGallery = ({isEdit, photos, changePhoto}) => {
                 cols={item?.cols}
                 rows={item?.rows}
                 sx = {{position: 'relative'}}
+                onClick={openImageDialog(item?.src)}
+
               >
 
                 {isEdit && (
@@ -65,13 +81,16 @@ const PhotoGallery = ({isEdit, photos, changePhoto}) => {
                   :
                   // TODO: Add a placeholder image
                   <div style={{ height: '100%', width: '100%', display:'flex', justifyContent:'center', alignItems:'center', backgroundColor: 'lightgrey', fontSize: '48px' }}>
-                    <Image fontSize='inherit' />
+                    <ImageIcon fontSize='inherit' />
                   </div>
                 }
               </ImageListItem>
             );
           })}
         </ImageList>
+        <LightBox closeImageDialog={closeImageDialog} image={selectedImage} />
+
+
       </Box>
     </Box>
   );
@@ -79,3 +98,72 @@ const PhotoGallery = ({isEdit, photos, changePhoto}) => {
 
 export default PhotoGallery;
 
+const LightBox = ({closeImageDialog, image}) => {
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
+  const [hasCalculated, setHasCalculated] = useState(false);
+
+  const getImageDimensions = (src) => {
+    return new Promise((resolve) => {
+      let img = new Image();
+      img.onload = () => resolve({ width: img.width, height: img.height });
+      img.src = src;
+    });
+  };
+
+  useEffect(() => {
+    if (image) {
+      setHasCalculated(false);
+      getImageDimensions(image).then((dims) => {
+        setImageSize(dims);
+        setHasCalculated(true);
+      });
+    }
+  }, [image]);
+
+  const isPortrait = imageSize.height > imageSize.width;
+
+  return (
+    <Dialog
+      open={!!image}
+      onClose={closeImageDialog}
+      fullScreen
+      sx = {{
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+      }}
+      PaperProps={{
+        style: {
+          backgroundColor: 'transparent',
+          boxShadow: 'none',
+        },
+      }}
+      onClick={closeImageDialog}
+    >
+      <div 
+        style={{
+          backgroundColor: 'transparent',
+          margin: 'auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: isPortrait?'90vh':"auto", 
+          width: isPortrait?'auto':"90vw",
+        }}
+      >
+
+        {hasCalculated &&
+          <img 
+            src={image} 
+            alt="dialog" 
+            style={{
+              maxHeight: isPortrait ? '100%' : '90vh',
+              maxWidth: isPortrait ? 'auto' : '100%',
+              objectFit: 'contain'
+            }}
+          />
+        }
+      </div>
+    </Dialog>
+  );
+};
