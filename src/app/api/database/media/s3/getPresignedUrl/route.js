f98aff4374faca73da7cfc4a2bb658ba21876019
jado66 <77120464+jado66@users.noreply.github.com>
2024-05-16 14:53:18 -0600

    Printing env variables to determine mismatch

--- src/app/api/database/media/s3/getPresignedUrl/route.js
+++ src/app/api/database/media/s3/getPresignedUrl/route.js
@@ -5,8 +5,7 @@ import { v4 as uuidv4 } from 'uuid'
 export async function POST(request) {
   const { filename, contentType } = await request.json()
 
-  console.log('Region:', process.env.AWS_REGION);
-  console.log('Bucket:', process.env.AWS_BUCKET_NAME);
+  console.log('All Env Variables:', process.env);
 
   try {
     const client = new S3Client({ region: process.env.AWS_REGION })
