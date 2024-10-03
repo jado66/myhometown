c96ed976899c57c8cc0211e6ad46cf55fc55a991
jadon-erwin-sinch <jadon.erwin@sinch.com>
2024-10-03 15:29:33 -0600

    Added directory to the staging

--- src/layout/navigation.js
+++ src/layout/navigation.js
@@ -31,14 +31,14 @@ export const pages = () => {
         //   title: 'Email Communications',
         //   href:  rootUrl+'/tools/email',
         // },
-        // {
-        //   title: "Texting",
-        //   href: rootUrl + "/admin-dashboard/tools/sms",
-        // },
-        // {
-        //   title: "Contact Directory",
-        //   href: rootUrl + "/admin-dashboard/tools/directory",
-        // },
+        {
+          title: "Texting",
+          href: rootUrl + "/admin-dashboard/tools/sms",
+        },
+        {
+          title: "Contact Directory",
+          href: rootUrl + "/admin-dashboard/tools/directory",
+        },
       ],
     },
     {
