e25bae1e61b29220a132eb959d25f738b0f34a69
jado66 <jado66@gmail.com>
2025-02-14 14:00:09 -0700

    hardcoded url??

--- src/app/api/auth/password-reset/route.js
+++ src/app/api/auth/password-reset/route.js
@@ -100,7 +100,7 @@ export async function POST(request) {
       type: "recovery",
       email,
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_DOMAIN}/auth/reset-password`,
+        redirectTo: `https://myhometownut.com/auth/reset-password`,
       },
     });
 
