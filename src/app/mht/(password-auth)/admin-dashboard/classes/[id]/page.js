476fc8fd56d60d359e117643554fb466210f2152
jado66 <jado66@gmail.com>
2025-10-15 18:52:29 -0600

    Fixed classes

--- src/app/mht/(password-auth)/admin-dashboard/classes/[id]/page.js
+++ src/app/mht/(password-auth)/admin-dashboard/classes/[id]/page.js
@@ -12,6 +12,27 @@ import { useRouter } from "next/navigation";
 import Loading from "@/components/util/Loading";
 import { useUser } from "@/hooks/use-user";
 
+export const newToOldCommunity = {
+  "a78e8c7c-eca4-4f13-b6c8-e5603d1c36da": "66a811814800d08c300d88fd", // Orem - Geneva Heights
+  "a6c19a50-7fc3-4759-b386-6ebdeca3ed9e":
+    "fb34e335-5cc6-4e6c-b5fc-2b64588fe921", // Orem - Sharon Park
+  "b3381b98-e44f-4f1f-b067-04e575c515ca": "66df56bef05bd41ef9493f33", // Provo - Pioneer Park
+  "7c446e80-323d-4268-b595-6945e915330f": "66df56e6f05bd41ef9493f34", // Provo - Dixon
+  "7c8731bc-1aee-406a-9847-7dc1e5255587": "66df5707f05bd41ef9493f35", // Provo - South Freedom
+  "0806b0f4-9d56-4c1f-b976-ee04f60af194": "66df577bf05bd41ef9493f37", // Ogden - North
+  "bf4a7d58-b880-4c18-b923-6c89e2597c71": "66df5790f05bd41ef9493f38", // Ogden - South
+  "0bdf52a4-2efa-465b-a3b1-5ec4d1701967": "66df57a2f05bd41ef9493f39", // Ogden - West
+  "995c1860-9d5b-472f-a206-1c2dd40947bd": "66df57b3f05bd41ef9493f3a", // Salt Lake City - Central
+  "af0df8f5-dab7-47e4-aafc-9247fee6f29d": "66df57c2f05bd41ef9493f3b", // Salt Lake City - Northwest
+  "5de22b0b-5dc8-4d72-b424-95b0d1c94fcc": "66df57d1f05bd41ef9493f3c", // Salt Lake City - Westside
+  "252cd4b1-830c-4cdb-913f-a1460f218616": "66df57e6f05bd41ef9493f3d", // West Valley City - Central Granger
+  "7d059ebc-78ee-4b47-97ab-276ae480b8de": "6838adb32243dc8160ce207d", // Layton - Layton
+  "4687e12e-497f-40a2-ab1b-ab455f250fce": "66df57faf05bd41ef9493f3e", // West Valley City - North East Granger
+  "2bc57e19-0c73-4781-9fc6-ef26fc729847": "66df580bf05bd41ef9493f3f", // West Valley City - West Granger
+  "0076ad61-e165-4cd0-b6af-f4a30af2510c": "66df581af05bd41ef9493f40", // West Valley City - Central Valley View
+  "724b1aa6-0950-40ba-9453-cdd80085c5d4": "6876c09a2a087f662c17feed", // Santaquin - Santaquin
+};
+
 export default function ClassPage({ params }) {
   const { getCommunity, loading: communityLoading } = useCommunities();
   const { getClassesByCommunity, loading: classesLoading } = useClasses();
@@ -39,9 +60,11 @@ export default function ClassPage({ params }) {
     const fetchData = async () => {
       try {
         // Fetch both community and classes data
-        const communityData = await getCommunity(params.id);
-        const classesData = await getClassesByCommunity(params.id);
 
+        const oldCommunityId = newToOldCommunity[params.id] || params.id;
+
+        const communityData = await getCommunity(oldCommunityId);
+        const classesData = await getClassesByCommunity(oldCommunityId);
         if (communityData && classesData) {
           // Create a map of classes for quick lookup
           const classesMap = new Map(
@@ -99,8 +122,9 @@ export default function ClassPage({ params }) {
   const refetchCommunityData = async () => {
     setLoading(true);
     try {
-      const communityData = await getCommunity(params.id);
-      const classesData = await getClassesByCommunity(params.id);
+      const oldCommunityId = newToOldCommunity[params.id] || params.id;
+      const communityData = await getCommunity(oldCommunityId);
+      const classesData = await getClassesByCommunity(oldCommunityId);
       if (communityData && classesData) {
         const classesMap = new Map(
           classesData.map((classItem) => [classItem.id, classItem])
