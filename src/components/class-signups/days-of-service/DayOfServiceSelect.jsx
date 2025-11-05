59312fb72a9a1cdeb82f9d51fb6c5577ab0296ee
jado66 <jado66@gmail.com>
2025-11-04 17:25:27 -0700

    Fixed the Days Of Service Volunteer page

--- src/components/class-signups/days-of-service/DayOfServiceSelect.jsx
+++ src/components/class-signups/days-of-service/DayOfServiceSelect.jsx
@@ -10,7 +10,6 @@ import {
 } from "@mui/material";
 import { useParams } from "next/navigation";
 import { useDaysOfService } from "@/hooks/useDaysOfService";
-import { communityIdMap } from "@/util/communityIdMap";
 import { useDayOfServiceId } from "@/contexts/DayOfServiceIdProvider";
 import moment from "moment";
 import { useClassSignup } from "../ClassSignupContext";
@@ -50,13 +49,9 @@ export const DayOfServiceSelect = ({
           .replaceAll(/-/g, " ")
           .replaceAll(/%20/g, " ");
 
-        const key = `${city}-${community}`;
+        const key = `${community}`;
 
-        const communityId = communityIdMap[key];
-
-        if (!communityId) {
-          throw new Error(`Community not found for: ${key}`);
-        }
+        const communityId = keyToCommunityId[key];
 
         const { data } = await fetchDaysOfServiceByCommunity(communityId);
 
@@ -133,4 +128,24 @@ export const DayOfServiceSelect = ({
       </FormControl>
     </Box>
   );
+};
+
+export const keyToCommunityId = {
+  "geneva-heights": "a78e8c7c-eca4-4f13-b6c8-e5603d1c36da",
+  "pioneer-park": "b3381b98-e44f-4f1f-b067-04e575c515ca",
+  dixon: "7c446e80-323d-4268-b595-6945e915330f",
+  "south-freedom": "7c8731bc-1aee-406a-9847-7dc1e5255587",
+  north: "0806b0f4-9d56-4c1f-b976-ee04f60af194",
+  south: "bf4a7d58-b880-4c18-b923-6c89e2597c71",
+  west: "0bdf52a4-2efa-465b-a3b1-5ec4d1701967",
+  central: "995c1860-9d5b-472f-a206-1c2dd40947bd",
+  northwest: "af0df8f5-dab7-47e4-aafc-9247fee6f29d",
+  westside: "5de22b0b-5dc8-4d72-b424-95b0d1c94fcc",
+  "central-granger": "252cd4b1-830c-4cdb-913f-a1460f218616",
+  "north-east-granger": "4687e12e-497f-40a2-ab1b-ab455f250fce",
+  "west-granger": "2bc57e19-0c73-4781-9fc6-ef26fc729847",
+  "central-valley-view": "0076ad61-e165-4cd0-b6af-f4a30af2510c",
+  "sharon-park": "a6c19a50-7fc3-4759-b386-6ebdeca3ed9e",
+  layton: "7d059ebc-78ee-4b47-97ab-276ae480b8de",
+  santaquin: "724b1aa6-0950-40ba-9453-cdd80085c5d4",
 };
